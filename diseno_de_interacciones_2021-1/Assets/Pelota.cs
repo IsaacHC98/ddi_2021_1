a41548c498706b3b242d66372f5a0cb700b999fc
Isaac Hernandez <isaac.hernandez.cano@uabc.edu.mx>
2021-02-12 17:49:01 -0800

    agregue el proyecto de la pelota

---
+++ diseno_de_interacciones_2021-1/Assets/Pelota.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pelota : MonoBehaviour
+{
+    public Vector3 direction;
+    public Vector3 torque;
+    public float speed;
+    public float magnusScale = 10;
+    private Rigidbody rb;
+    private float intensity;
+
+    void Start()
+    {
+        Debug.Log("Hola mundo");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(direction * Time.deltaTime * speed);
+        if(Input.GetKeyDown(KeyCode.P)){
+            rb.AddForce(direction * speed, ForceMode.Force);
+            rb.AddTorque(torque);
+        }
+        Vector3 velocity = rb.velocity;
+        Vector3 angularVelocity = rb.angularVelocity;
+        intensity = 2 * Mathf.PI * magnusScale;
+        Vector3 magnus = Vector3.Cross(velocity, angularVelocity * intensity);
+        rb.AddForce(magnus);
+    }
+}
