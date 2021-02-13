90f2dc79fbeccb417b3d6dfac38e1a8f9ada3367
Isaac Hernandez <isaac.hernandez.cano@uabc.edu.mx>
2021-02-13 12:55:03 -0800

    agregue comentario del orden del algoritmo

--- diseno_de_interacciones_2021-1/Assets/CodeProblems/Problem1.cs
+++ diseno_de_interacciones_2021-1/Assets/CodeProblems/Problem1.cs
@@ -10,15 +10,17 @@ public class Problem1 : MonoBehaviour
         int[] nums = {8,1,2,2,3};
         int[] nums2 = {-8,5,6,-10,20};
         int[] nums3 = {7,-6,18,3,-15};
-        int[] output = NumbersLessThan(nums3);
+        int[] output = NumbersLessThan(nums);
         Debug.Log("[" + string.Join(",", new List<int>(output).ConvertAll(i => i.ToString()).ToArray()) + "]");
     }
 
+    //EL orden del algoritmo es O(n^2)
+    ///Esto es porque en el algoritmo se usan ciclos anidados.
     private int[] NumbersLessThan(int[] nums){
         int[] result = {0,0,0,0,0};
 
-        for(int i=0; i<5; i++){
-            for(int j=0; j<5; j++){
+        for(int i=0; i<nums.Length; i++){
+            for(int j=0; j<nums.Length; j++){
                 if(nums[i]>nums[j]){
                     result[i]++;
                 }
