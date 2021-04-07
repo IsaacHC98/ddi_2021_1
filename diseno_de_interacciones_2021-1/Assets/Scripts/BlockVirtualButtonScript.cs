5dc425f07657100d6e4a331acbc4647b11e2ff3a
Isaac Hernandez <isaac.hernandez.cano@uabc.edu.mx>
2021-04-07 15:50:41 -0700

    update con los virtual buttons

---
+++ diseno_de_interacciones_2021-1/Assets/Scripts/BlockVirtualButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class BlockVirtualButtonScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject augmentable;
+    private VirtualButtonBehaviour virtualButton;
+    public Animator animator;
+
+    private void Awake() {
+        virtualButton = GetComponent<VirtualButtonBehaviour>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(virtualButton != null){
+            virtualButton.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        animator.SetTrigger("block");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+    }
+}
---
+++ diseno_de_interacciones_2021-1/Assets/Scripts/VirtualButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject augmentable;
+    private VirtualButtonBehaviour virtualButton;
+    public Animator animator;
+
+    private void Awake() {
+        virtualButton = GetComponent<VirtualButtonBehaviour>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(virtualButton != null){
+            virtualButton.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        animator.SetTrigger("attack");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+    }
+}
